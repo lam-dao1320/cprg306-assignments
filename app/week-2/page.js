3e579642d2b4dcd8935cfbf668d6c5cccc7bc10c
Lam Dao <lam.dao@edu.sait.ca>
2025-05-13 10:01:45 -0600

    Set up for week 2 assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function WeekTwoPage() {
+    return (
+        <div>
+            <p>Week Two Assignment</p>
+            <Link href="/">Home</Link>
+        </div>
+    );
+}
